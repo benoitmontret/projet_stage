f13a9b08d08b2cb85f64693959a914d8e35ffe24
benoit montret <benoit.montret@gmail.com>
2024-03-19 11:42:06 +0100

    procedure de login avec test

--- login.php
+++ login.php
@@ -1,6 +1,8 @@
 <?php
+ob_start();
 $user = null;
 $prev_page = $_COOKIE['prev'];
+include ("db.php");
 
 if (!empty($_GET['action']) && $_GET['action'] === 'deconnecter') {
     unset($_COOKIE['user']);
@@ -10,13 +12,27 @@ if (!empty($_COOKIE['user'])) {
     $user = $_COOKIE['user'];
 }
 if (!empty($_POST['nom'])) {
-    setcookie('user', $_POST['nom'], time()+3600); //3600=1h modifier selon besoin
-    $user=$_POST['nom'];
+    $res = explode('|', $_POST['nom']); //$rres[0] contient le nom+prenom, res[1] l'id bénévol
+    $sql = "SELECT dt_nais FROM benevoles WHERE id_bnv=?";
+    $req = $db->prepare($sql);
+    $req -> bindvalue(1,$res[1],PDO::PARAM_INT);
+    $req->execute();
+        $resultat= $req->fetch();
+    echo $resultat[0].'<br>'.$_POST['dt_nais'];
+    if ($resultat[0]!=$_POST['dt_nais']) {
+        echo '<p class="log_err">Erreur : Le nom et la date de naissance ne correspondent pas !!!</p>';
+    } else {
 
+        // echo "nom: ". $res[0]."<br />";
+        // echo "id: ". $res[1]."<br />";
+        setcookie('user', $res[0], time()+3600); //3600=1h modifier selon besoin
+        $user=$res[0];
+    }
 }
-
-include ("db.php");
+$test = ob_get_clean();
 include("header.php");
+echo $test;
+
 
 if ($user): ?>
 <div class="log_status">
@@ -37,7 +53,7 @@ if ($user): ?>
             
 
             <?php while ($row = $req->fetch()) { ?>
-                <option value="<?php echo $row['nom']." ".$row["pnom"]; ?>" 
+                <option value="<?php echo $row['nom']." ".$row["pnom"]."|".$row["id_bnv"]; ?>" 
                 ><?php echo $row['nom']." ".$row["pnom"]; ?>
                 </option>
             <?php } ?>
